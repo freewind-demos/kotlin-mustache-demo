c190e99102cd0cd970be40c6ec81cb1cc1b5074d
freewind <nowindlee@gmail.com>
2018-07-28 23:01:29 +0800

    add list of string demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -5,8 +5,8 @@ import java.io.PrintWriter
 
 fun main(args: Array<String>) {
     val page = Page(listOf(
-            Item("Item 1", "$19.99", listOf(Feature("New!"), Feature("Awesome!"))),
-            Item("Item 2", "$29.99", listOf(Feature("Old."), Feature("Ugly.")))
+            Item("Item 1", "$19.99", listOf("new", "good"), listOf(Feature("New!"), Feature("Awesome!"))),
+            Item("Item 2", "$29.99", listOf("old", "bad"), listOf(Feature("Old."), Feature("Ugly.")))
     ))
     val mf = DefaultMustacheFactory()
     val mustache = mf.compile(mf.getReader("templates/hello.html"), "hello")
@@ -17,4 +17,4 @@ data class Page(val items: List<Item>)
 
 data class Feature(val description: String)
 
-data class Item(val name: String, val price: String, val features: List<Feature>)
+data class Item(val name: String, val price: String, val keywords: List<String>, val features: List<Feature>)
